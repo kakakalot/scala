e3e4c830547b7bddfdfe9aa5ede48a66e0cc9708
tran ngoc chan thien <thien.tnc@vn.gnt-global.com>
2016-06-29 11:49:16 +0700

    update source image, json

--- build.sbt
+++ build.sbt
@@ -5,5 +5,7 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "org.scala-lang" % "scala-reflect" % "2.11.8"
+  "org.scala-lang" % "scala-reflect" % "2.11.8",
+  "com.sksamuel.scrimage" %% "scrimage-core" % "2.1.0",
+  "com.typesafe.play" % "play-json_2.11" % "2.4.2"
 )
--- src/main/scala/Debug.scala
+++ src/main/scala/Debug.scala
@@ -1,4 +1,9 @@
 
+import java.io.File
+
+import com.sksamuel.scrimage.Image
+import play.api.libs.json._
+
 import scala.reflect.runtime.universe._
 import scala.reflect.runtime.currentMirror
 
@@ -21,7 +26,8 @@ object Debug extends App {
 //    obj.setV("d", Seq(1,2,3))
 //    obj.setV("a", "abc")
 
-    for {
+    var _map: Map[String, Any] = Map()
+    val result = for {
       _d <- _update
       member <- reflectedInstance.symbol.toType.members
       if member.isPublic && (!member.isMethod || member.asMethod.isGetter)
@@ -30,16 +36,36 @@ object Debug extends App {
         if (member.name == _d._1) {
           reflectedInstance.reflectMethod(member.asMethod)(_d._2)
         }
+
         val value = reflectedInstance.reflectMethod(member.asMethod)()
-        println(member.name, value)
+        _map += (member.name.toString -> value)
       }
       catch {
         case e:Exception =>
           e.printStackTrace()
       }
-
-      Obj("", Some(""), new C, Seq(), Some(false))
     }
+    implicit def writeAny: Writes[Map[String, Any]] = new Writes[Map[String, Any]] {
+      override def writes(o: Map[String, Any]): JsValue = {
+        var _sep:Seq[(String, JsValue)] = List()
+        for (key <- o.keySet) {
+          _sep++= Seq("a" -> JsNumber(1))
+        }
+
+        JsObject(_sep)
+      }
+    }
+
+
+    println(Json.toJson(_map))
+//    Obj("", Some(""), new C, Seq(), Some(false))
+//    val a = result.fold {
+//
+//    }
+
+
+    val inImage = Image.fromFile(new File("D:\\screen_shot.jpg"))
+    println(s"w: ${inImage.width}, h: ${inImage.height}")
 
   }
 }
